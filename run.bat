00189b42a87700e076e44fb6dfc04bbc05828102
Nikita Vasilyev <v.nik-97@bk.ru>
2017-11-22 17:30:36 +0300

    Count transformed

--- run.bat
+++ run.bat
@@ -5,11 +5,12 @@ for /R %CD%\old %%f IN (*.gr2) do (
 preprocessor StripImages old\%%~nxf -output new\%%~nxf
 preprocessor Compress new\%%~nxf -output new\%%~nxf
 preprocessor KillUncommonData new\%%~nxf -output new\%%~nxf
+set /a count+=1
 echo %%~nxf "->" Transformed
 )
-echo ----------------------+----------------------
-echo All successfully Transformed to granny 2.11.8
+echo ----------------------------+----------------------------
+echo %count% Models successfully Transformed to granny 2.11.8
 echo New files will be in the folder "new"
 echo With love from nikita322 :D
-echo ----------------------+----------------------
+echo ----------------------------+----------------------------
 PAUSE
